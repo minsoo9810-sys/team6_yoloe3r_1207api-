244e24efcd7d0bb798f8ff3b74112b8a7cf3b019
Hyeonju Cho <chohjeonju5640@gmail.com>
2025-12-03 12:33:20 +0900

    Update main_report.py

--- modules/llm_final_api/main_report.py
+++ modules/llm_final_api/main_report.py
@@ -1,38 +1,23 @@
-import time
 import json
-from .config import *
-from .report.utils.image_selector import select_best_image
-from .report.utils.report_parser import parse_report_output
-from .report.report_client import run_report_model
-from .report.report_prompt import report_prompt
-from ultralytics import YOLOE
+from config import *
+from report.utils.image_selector import select_best_image
+from report.utils.report_parser import parse_report_output
+from report.report_client import run_report_model
+from report.report_prompt import report_prompt
 
 
-def main_report(img_path):
+def main():
     # ----- 1단계: 3장의 이미지 중 최적의 입력 이미지 1장 선택 ------
-    model = YOLOE("yoloe-11s-seg.pt")
-    max_cnt=0
-    maxidx = 0
-    for i, img in enumerate(img_path):
-        results = model.predict(img)
-        if len(results[0].boxes)>max_cnt:
-            maxidx = i
-            max_cnt=len(results[0].boxes)
-        else:
-            pass
+    final_input_path = select_best_image(
+        api_key=API_KEY, 
+        model_name=REPORT_MODEL,        # 리포트는 Gemini-2.5-flash 사용
+        input_paths=INITIAL_IMAGE_PATHS,    # config에 정의된 사용자가 입력한 3장의 이미지
+        selected_output_path=SELECTED_IMAGE_PATH,
+    )
 
-    # final_input_path = select_best_image(
-    #     api_key=API_KEY, 
-    #     model_name=REPORT_MODEL,        # 리포트는 Gemini-2.5-flash 사용
-    #     input_paths=img_path,    # ui 에서 입력한 이미지
-    #     selected_output_path=SELECTED_IMAGE_PATH,
-    # )
-
-    # if not final_input_path:
-    #     print("유효한 입력 이미지를 확인하세요.")
-    #     return
-
-    print('최적 입력 이미지 : '+img_path[maxidx])
+    if not final_input_path:
+        print("유효한 입력 이미지를 확인하세요.")
+        return
 
     # ------ 2단계: 공간 분석 리포트 생성 ------
     try:
@@ -40,12 +25,9 @@ def main_report(img_path):
         raw_report_text = run_report_model(
             api_key=API_KEY,
             model_name=REPORT_MODEL, # 리포트는 Gemini-2.5-flash 사용
-            image_path=img_path[maxidx],  # 1단계에서 선택된 이미지 사용
+            image_path=final_input_path,  # 1단계에서 선택된 이미지 사용
             prompt=report_prompt,
         )
-
-        # Gemini 응답 대기 시간
-        time.sleep(1)
 
         # 전체 리포트 파싱
         parsed_data = parse_report_output(raw_report_text)
@@ -69,4 +51,4 @@ def main_report(img_path):
 
 
 if __name__ == "__main__":
-    main_report()
+    main()
